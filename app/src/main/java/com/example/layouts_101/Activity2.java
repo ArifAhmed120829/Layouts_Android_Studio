89dfa220d66d3b7e971a00c9efc4068b4dd75d1c
ArifAhmed <arif15-12301@diu.edu.bd>
2022-01-28 19:49:30 +0600

    Layouts

---
+++ app/src/main/java/com/example/layouts_101/Activity2.java
@@ -0,0 +1,15 @@
+package com.example.layouts_101;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lineary);
+
+    }
+}
--- app/src/main/java/com/example/layouts_101/MainActivity.java
+++ app/src/main/java/com/example/layouts_101/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.layouts_101;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button100);
+        Button button2 = findViewById(R.id.button101);
+        Button button3 = findViewById(R.id.button102);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(view.getContext(),Activity2.class);
+                startActivity(i);
+
+            }
+
+        }
+
+        );
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i2 = new Intent(view.getContext(),Activity3.class);
+                startActivity(i2);
+
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i3 = new Intent(view.getContext(),Activity4.class);
+                startActivity(i3);
+
+            }
+        });
+
+
     }
 }
